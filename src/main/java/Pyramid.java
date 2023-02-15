e008906b3988c70f95085d9c3a6c36788b924044
Devynn R. Palmer <palmerdevynn@gmail.com>
2023-02-15 21:42:31 +0000

    pyramid lab

--- src/main/java/Pyramid.java
+++ src/main/java/Pyramid.java
@@ -28,6 +28,14 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+        return sb.toString();
     }
 }
